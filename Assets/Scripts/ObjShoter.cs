6d97ef8d418cd0b11ed2fd8a798bc6bff25767b2
GiGi <b9930084@gmail.com>
2019-04-26 14:49:18 +0800

    shoter

---
+++ Assets/Scripts/ObjShoter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjShoter : MonoBehaviour
+{
+
+    public GameObject Obj;
+    GameObject Objcopy;
+    private float nt, bt;
+
+    Vector3 shoterPos;
+
+    public Vector3 Upvector;
+    public float Xvector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shoterPos = transform.position;
+        nt = Time.time;
+        bt = nt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nt = Time.time;
+        shoterPos = transform.position;
+        if ((nt - bt) >= 0.5)
+        {
+            bt = nt;
+            shoterPos.x = Random.Range(shoterPos.x - 1.5f, shoterPos.x +1.5f);
+            //shoterPos.y = Random.Range(shoterPos.y - 1.5f, shoterPos.y + 1.5f);
+            //shoterPos.z = Random.Range(shoterPos.z - 1.5f, shoterPos.z + 1.5f);
+            Objcopy = GameObject.Instantiate(Obj, shoterPos, Quaternion.identity);
+            Objcopy.GetComponent<Rigidbody>().isKinematic = false;
+            Upvector.x = Random.Range(-60f, 60f);
+            Upvector.y = Random.Range(380f, 420f);
+            Upvector.z = 500;
+            Objcopy.GetComponent<Rigidbody>().AddForce(Upvector);
+            Objcopy.GetComponent<Rigidbody>().AddTorque(Random.Range(-100f, 100f), Random.Range(-100f, 100f), Random.Range(-100f, 100f));
+
+        }
+
+    }
+}
